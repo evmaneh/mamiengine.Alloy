d4873ca39e6a3f59a8c897ed570c6267e71311a5
Jayson <122318862+shufu420@users.noreply.github.com>
2023-12-20 17:27:35 +0000

    Update 3 files

--- public/assets/js/surf.js
+++ public/assets/js/surf.js
@@ -3,36 +3,43 @@
 /* How to use? : Check the documentation. Button ID attributes are used for the script below.
 /* v2.0.0
 /* ----------------------------------------------- */
-$ = e => document.getElementById(e) || [];
+$ = (e) => document.getElementById(e) || [];
 
-$("tacogithub").href = "https://github.com/Tacosheel/TacoProxy";
+$("tacogithub").href = "https://github.com/evmaneh/mamiengine.Alloy";
 
-//Alloy Default
-
-$('alloyframe').onclick = function() {
-    var frame = document.getElementById("frame");
-    var url = $('iurl').value;
-    var det = document.domain;
-    var domain = det.replace('www.', '').split(/[/?#]/)[0];
-    const origin = btoa(url)
-    frame.src = "https://" + domain + "/prefix/" + origin;
-    frame.style['visibility'] = "visible";
-    return false;
-};
-
-//Alloy Frame
-
-$('alloydefault').onclick = function() {
-    var frame = document.getElementById("frame");
-    var url = $('iurl').value;
-    var det = document.domain;
-    var domain = det.replace('www.', '').split(/[/?#]/)[0];
-    const origin = btoa(url)
-    window.location.href = "https://" + domain + "/prefix/" + origin;
-    return false;
-};
-
-
-window.onload = function() {
-    $('iurl').focus();
+// Function to add "https://" if not present and check if it's a URL
+function formatURL(url) {
+  if (url.match(/^https?:\/\//i)) {
+    return url; // It's already a URL
+  } else {
+    // It's not a URL, perform Google search
+    return "https://www.google.com/search?q=" + encodeURIComponent(url);
+  }
 }
+
+// Alloy Default
+$("alloyframe").onclick = function () {
+  var frame = document.getElementById("frame");
+  var url = formatURL($("iurl").value); // Format the URL or perform Google search
+  var det = document.domain;
+  var domain = det.replace("www.", "").split(/[/?#]/)[0];
+  const origin = btoa(url);
+  frame.src = "https://" + domain + "/prefix/" + origin;
+  frame.style["visibility"] = "visible";
+  return false;
+};
+
+// Alloy Frame
+$("alloydefault").onclick = function () {
+  var frame = document.getElementById("frame");
+  var url = formatURL($("iurl").value); // Format the URL or perform Google search
+  var det = document.domain;
+  var domain = det.replace("www.", "").split(/[/?#]/)[0];
+  const origin = btoa(url);
+  window.location.href = "https://" + domain + "/prefix/" + origin;
+  return false;
+};
+
+window.onload = function () {
+  $("iurl").focus();
+};
